96dd11d4f707963594b06f22bf9c8d2d358d8fce
64-bit-david <david.dev.w@gmail.com>
2022-11-20 14:55:43 +0000

    change to db model for word_frequency column to Text

--- TimedTrigger/TimedTrigger/models.py
+++ TimedTrigger/TimedTrigger/models.py
@@ -9,7 +9,7 @@ class HourlyWordFrequency(Base):
    __tablename__ = 'wf24'
    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
    website = sqlalchemy.Column(sqlalchemy.String(length=100))
-   word_frequency = sqlalchemy.Column(sqlalchemy.JSON, nullable=True)
+   word_frequency = sqlalchemy.Column(sqlalchemy.Text, nullable=True)
    updated_at= sqlalchemy.Column(sqlalchemy.TIMESTAMP, nullable=True)
    #might need to update the text insert, test fails because of it i think
 #    updated_at= sqlalchemy.Column(sqlalchemy.TIMESTAMP, nullable=False, server_default=sqlalchemy.text('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'))
